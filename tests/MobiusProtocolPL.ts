b0b3788fa912bfbac172aa55a413f030756bbf63
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-06-27 11:06:52 +0800

    updating MobiusProtocolPL.ts
    creating contributor and fundraiser accounts for testing

--- tests/MobiusProtocolPL.ts
+++ tests/MobiusProtocolPL.ts
@@ -6,6 +6,70 @@ import { TOKEN_PROGRAM_ID, createMint, createAssociatedTokenAccount, mintTo, get
 import chai, { assert, AssertionError, expect } from 'chai';
 import chaiAspromised from 'chai-as-promised';
 
-import { }
+import { MorbuisClient } from '../src';
+
+chai.use(chaiAspromised);
+
+describe("morbius", () => {
+  //configure the client to use the local cluster 
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const th = new MorbuisClient(provider.connection, provider.wallet as any)
+
+  let fundraiserTokenAccount = null;
+  let contributor1TokenAccount = null;
+  let contributor2TokenAccount = null;
+  let contributor3TokenAccount = null;
+
+  const fundraiser = anchor.web3.Keypair.generate();
+  const contributor1 = anchor.web3.Keypair.generate();
+  const contributor2 = anchor.web3.Keypair.generate();
+  const contributor3 = anchor.web3.Keypair.generate();
+
+  const fundraiserConfig = anchor.web3.Keypair.generate();
+
+  it("Initialize token accounts", async () => {
+    //airdrop to fundraiser 
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(fundraiser.publicKey, 1000000000),
+      "confirmed"
+    );
+
+    //fund contributors account
+    await provider.sendAndConfirm(
+      (() => {
+        const tx = new Transaction();
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: fundraiser.publicKey,
+            toPubkey: contributor1.publicKey,
+            lamports: 100000000,
+          }),
+          SystemProgram.transfer({
+            fromPubkey: fundraiser.publicKey,
+            toPubkey: contributor2.publicKey,
+            lamports: 100000000,
+          }),
+          SystemProgram.transfer({
+            fromPubkey: fundraiser.publicKey,
+            toPubkey: contributor3.publicKey,
+            lamports: 100000000,
+          }),
+        );
+        return tx;
+      })(),
+      [fundraiser]
+    );
+    // create host & players reward token accounts
+    hostTokenAccountReward = await createAssociatedTokenAccount(
+      provider.connection,
+      host,
+      mintReward,
+      host.publicKey
+    );
+  });
 
 
+
+});
