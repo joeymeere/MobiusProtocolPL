42b836387a96e0b1f9cbcff04b4a4c1524e7a332
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-06-27 14:54:05 +0800

    updated index.ts for tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from '@solana/web3.js';
 
-export * from './tradehaus';
-export * from './tradehaus-common';
+export * from './morbius';
+export * from './morbius-common';
 
 export const TRADEHAUS_PROG_ID = new PublicKey(
     'Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS'
